43e0fc4ecdfe09d94fc392ca1fc8adf4e81b75a0
lennnnnnnnnnnny <helen.hachtel@gmail.com>
2026-01-18 21:50:32 -0500

    Update index.ts

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -4,11 +4,13 @@ import {
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
 import * as whatsAlf from './whats-alf'
+import * as newAndNotable from './new-and-notable'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
   [whatsAlf.shortname]: whatsAlf.handler,
+  [newAndNotable.shortname]: newAndNotable.handler,
 }
 
 export default algos
